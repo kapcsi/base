a03c71be16609acc30755cd54d6f90539301efda
Kapcsi <kapcsi.work@gmail.com>
2021-03-01 16:46:41 +0100

    Modification of branch B

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -3,7 +3,7 @@ package hu.bme.mit.train.controller;
 import hu.bme.mit.train.interfaces.TrainController;
 
 public class TrainControllerImpl implements TrainController {
-
+        private int mod = 0;
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
